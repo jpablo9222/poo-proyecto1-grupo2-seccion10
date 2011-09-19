1a13999c3f8704aea2d70bb9ff2e9f9cc0682eff
jp_9222 <jp_9222@hotmail.com>
2011-09-19 00:03:32 +0000

    JavaDoc

--- Proyecto1/src/Prueba1/Contenedor.java
+++ Proyecto1/src/Prueba1/Contenedor.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Prueba1;
 
 /**
@@ -14,31 +10,56 @@ public class Contenedor {
     private ArrayList<Carga> cargas;
     private int codContenedor = 2011001;
     
+   /**
+    * Método Constructor.
+    * Crea un Contenedor de un Barco.
+    * @param capacidad Capacidad Máxima de Peso del Contenedor.
+    */
     public Contenedor(float capacidad)
     {
         codContenedor += 1; capacidad_maxima = capacidad; cargaActual = 0;
     }
     
+   /**
+    * Método que Devuelve la Capacidad Máxima de Peso del Contenedor.
+    * @return capacidad_maxima Capacidad Máxima de Peso del Contenedor.
+    */
     public float getCapacidad()
     {
         return capacidad_maxima;
     } 
     
+   /**
+    * Método que Devuelve el Código del Contenedor.
+    * @return codContenedor Código del Contenedor.
+    */
     public int getCodContendor()
     {
         return codContenedor;
     }
     
+   /**
+    * Método que Devuelve el Peso Actual del Contenedor.
+    * @return cargaActual Peso Actual del Contenedor.
+    */
     public float getCargaActual()
     {
         return cargaActual;
     }
     
+   /**
+    * Método que Establece el Peso Actual del Contenedor.
+    * @param x Peso Actual del Contenedor.
+    */
     public void setCargaActual(float x)
     {
         cargaActual = x;
     }
     
+   /**
+    * Método que Devuelve la Lista de Cargas Contenidas en el Contenedor.
+    * @return cargas Lista de Cargas Contenidas en el Contenedor.
+    */
     public ArrayList<Carga> getCarga()
     {
         return cargas;
