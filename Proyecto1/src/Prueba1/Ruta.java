aab2537f8383f635ee92060a2b79ec636a75ae1d
jp_9222 <jp_9222@hotmail.com>
2011-09-19 00:03:56 +0000

    JavaDoc

--- Proyecto1/src/Prueba1/Ruta.java
+++ Proyecto1/src/Prueba1/Ruta.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Prueba1;
 
 /**
@@ -9,29 +5,45 @@ package Prueba1;
  * @author Juan Pablo
  */
 import java.util.ArrayList;
-import java.util.Calendar; //asi se usan fechas
-// import java.text.SimpleDateFormat; Para darle formato a la fecha
+import java.util.Calendar;
 public class Ruta {
     private int codRuta;
     private ArrayList<Puerto> puertos;
     private ArrayList<Calendar> fecha_arribo;
     
+   /**
+    * Método Constructor.
+    * Crea la Ruta a Seguir de Un Barco.
+    * @param cod Código de la Ruta.
+    */
     public Ruta(int cod)
     {
         codRuta = cod; puertos = new ArrayList<Puerto>(); 
         fecha_arribo = new ArrayList<Calendar>();
     }
     
+   /**
+    * Método que Devuelve la Lista con los Puertos a Visitar.
+    * @return puertos Lista con los Puertos a Visitar.
+    */
     public ArrayList<Puerto> getPuerto()
     {
         return puertos;
     }
     
+   /**
+    * Método que Devuelve la Lista con las Fechas Programadas de Arribo a los Puertos a Visitar.
+    * @return fecha_arribo Lista con las Fechas Programadas de Arribo a los Puertos a Visitar.
+    */
     public ArrayList<Calendar> getFechaA()
     {
         return fecha_arribo;
     }
     
+   /**
+    * Método que Devuelve el Código de la Ruta.
+    * @return codRuta Código de la Ruta.
+    */
     public int getCodRuta()
     {
         return codRuta;
