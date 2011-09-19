a52980484fecd08a284495b114f5bfe0a8956880
jp_9222 <jp_9222@hotmail.com>
2011-09-19 00:05:40 +0000

    JavaDoc

--- Proyecto1/src/Prueba1/Puerto.java
+++ Proyecto1/src/Prueba1/Puerto.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Prueba1;
 
 /**
@@ -11,26 +7,49 @@ package Prueba1;
 public class Puerto {
     private String nombre, pais, coordenada;
     
+   /**
+    * Método Constructor.   
+    * Crea un Puertoo con la información proporcionada.
+    * @param name Nombre del Puerto.
+    * @param country Pais de Donde se Ubica el Puerto.
+    * @param coor Coordenadas de Ubicación del Puerto.
+    */
     public Puerto(String name, String country, String coor)
     {
         nombre = name; pais = country; coordenada = coor;
     }
     
+   /**
+    * Método que Devuelve el Nombre del Puerto.
+    * @return nombre Nombre del Puerto.
+    */
     public String getNombrePuerto()
     {
         return nombre;
     }
     
+   /**
+    * Método que Devuelve el Pais Donde se Ubica el Puerto.
+    * @return pais Pais Donde se Ubica el Puerto.
+    */
     public String getPaisPuerto()
     {
         return pais;
     }
     
+   /**
+    * Método que Devuelve la Coordenada de Ubicación del Puerto.
+    * @return coordenada Coordenada de Ubicación del Puerto.
+    */
     public String getCoordenadaPuerto()
     {
-        return nombre;
+        return coordenada;
     }
     
+   /**
+    * Método que Copia la Información de Otro Puerto.
+    * @param puertito Puerto a Copiar.
+    */
     public void copy(Puerto puertito)
     {
         nombre = puertito.nombre; pais = puertito.pais; 
