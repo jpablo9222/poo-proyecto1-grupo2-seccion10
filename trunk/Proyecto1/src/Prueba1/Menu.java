925204d445674f4234a9b49fb7712e5469963bc1
jp_9222@hotmail.com <jp_9222@hotmail.com@2356b6b5-4a5a-51e2-168c-11a8d753c739>
2011-09-19 07:03:45 +0000

    Arreglos Esteticos

--- trunk/Proyecto1/src/Prueba1/Menu.java
+++ trunk/Proyecto1/src/Prueba1/Menu.java
@@ -116,7 +116,7 @@ public class Menu {
     }
     
     static float estimarPeso(Barco barco, Puerto o){ 
-        float x = 0; int y = 0, z = 0;
+        float x = 0; int y = 0;
         for (Puerto puertito:barco.getRuta().getPuerto()){
             if (puertito.getNombrePuerto().equals(o.getNombrePuerto())){
                 y = barco.getRuta().getPuerto().indexOf(puertito);
@@ -124,15 +124,7 @@ public class Menu {
         }
         
         for (Carga producto:barco.getCargaProg()){
-            for (Puerto puertito:barco.getRuta().getPuerto()){
-                if (producto.getDestino().getNombrePuerto().equals(puertito.getNombrePuerto())){
-                    z = barco.getRuta().getPuerto().indexOf(puertito);
-                }       
-            }
             x+=producto.getPeso();
-            if (barco.getRuta().getFechaA().get(z).before(barco.getRuta().getFechaA().get(y))){
-                x-=producto.getPeso();
-            }
         }
         return x;
     }
