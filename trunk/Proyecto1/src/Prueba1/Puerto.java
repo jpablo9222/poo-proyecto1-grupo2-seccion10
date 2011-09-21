a30c088ae46f327519017420f4778090153d9274
jmunoz1991@gmail.com <jmunoz1991@gmail.com@2356b6b5-4a5a-51e2-168c-11a8d753c739>
2011-09-21 16:20:15 +0000

    Arreglos En Coordenadas

--- trunk/Proyecto1/src/Prueba1/Puerto.java
+++ trunk/Proyecto1/src/Prueba1/Puerto.java
@@ -5,7 +5,8 @@ package Prueba1;
  * @author Juan Pablo
  */
 public class Puerto {
-    private String nombre, pais, coordenada;
+    private String nombre, pais, EO,NS;
+    private Double coordenadaNS, coordenadaEO;
     
    /**
     * Método Constructor.   
@@ -14,9 +15,9 @@ public class Puerto {
     * @param country Pais de Donde se Ubica el Puerto.
     * @param coor Coordenadas de Ubicación del Puerto.
     */
-    public Puerto(String name, String country, String coor)
+    public Puerto(String name, String country, Double coorNS, Double coorEO, String EOs, String NSs)
     {
-        nombre = name; pais = country; coordenada = coor;
+        nombre = name; pais = country; coordenadaNS=coorNS; coordenadaEO=coorEO;EO=EOs; NS=NSs; 
     }
     
    /**
@@ -38,21 +39,23 @@ public class Puerto {
     }
     
    /**
-    * Método que Devuelve la Coordenada de Ubicación del Puerto.
-    * @return coordenada Coordenada de Ubicación del Puerto.
+    * MÃ©todo que Devuelve la Coordenada de UbicaciÃ³n del Puerto.
+    * @return coordenada Coordenada de UbicaciÃ³n del Puerto.
     */
     public String getCoordenadaPuerto()
     {
+        String coordenada="Latitud: "+ coordenadaNS+NS + "\t Longitud: "+coordenadaEO+EO;
         return coordenada;
     }
     
    /**
-    * Método que Copia la Información de Otro Puerto.
+    * MÃ©todo que Copia la InformaciÃ³n de Otro Puerto.
     * @param puertito Puerto a Copiar.
     */
     public void copy(Puerto puertito)
     {
+        String coordenada;
         nombre = puertito.nombre; pais = puertito.pais; 
-        coordenada = puertito.coordenada;
+        coordenada = puertito.getCoordenadaPuerto();
     }
 }
